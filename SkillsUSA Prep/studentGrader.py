f686d4a88e6b033c82e1912e7cf22fac915a66dc
Coby Nguyen <cobycn105@gmail.com>
2022-12-18 23:57:31 -0600

    Added more code

--- SkillsUSA Prep/studentGrader.py
+++ SkillsUSA Prep/studentGrader.py
@@ -1,2 +1,8 @@
 import os
 import math
+
+name = input("Enter your name: ")
+className = input("Enter your class: ")
+testScores = input("Enter your test scores: ")
+
+scores = [x for x in testScores if True]
