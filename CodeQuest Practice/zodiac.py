22abbe0f7bce2a2addfb637d4a0445d4f166a142
Coby Nguyen <cobycn105@gmail.com>
2023-04-06 23:33:00 -0500

    Updated files of prvious competyitons

---
+++ CodeQuest Practice/zodiac.py
@@ -0,0 +1,8 @@
+cases = int(input("Amount of test cases: "))
+
+for caseNum in range(cases):
+    numbers = input()
+    speed, distance = numbers.split(":")
+    SEXuntilCollision = distance/speed
+    if SEXuntilCollision <= 1
+        
